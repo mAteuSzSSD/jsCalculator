140dda0bd5c4778c779e20926b4ac1efafbb9087
mAteuSzSSD <82341023+mAteuSzSSD@users.noreply.github.com>
2022-12-06 20:26:58 +0100

    reworked js file
    basics of calculator works but there is a bug when you press the + - / x button

--- main.js
+++ main.js
@@ -1,78 +1,129 @@
-const btn0 = document.querySelector(".btn-0")
-const btn1 = document.querySelector(".btn-1")
-const btn2 = document.querySelector(".btn-2")
-const btn3 = document.querySelector(".btn-3")
-const btn4 = document.querySelector(".btn-4")
-const btn5 = document.querySelector(".btn-5")
-const btn6 = document.querySelector(".btn-6")
-const btn7 = document.querySelector(".btn-7")
-const btn8 = document.querySelector(".btn-8")
-const btn9 = document.querySelector(".btn-9")
-
-const allNumbers = document.querySelectorAll(".number")
-
-const btnReset = document.querySelector(".btn-reset")
-const btnPlusMinus = document.querySelector(".btn-plus-minus")
-const btnProcent = document.querySelector(".btn-procent")
-const btnDivideBy = document.querySelector(".btn-divide-by")
-const btnMultiply = document.querySelector(".btn-multiply")
-const btnMinus = document.querySelector(".btn-minus")
-const btnPlus = document.querySelector(".btn-plus")
-const btnEqualTo = document.querySelector(".btn-equal-to")
+const allButtons = document.querySelectorAll(".circle")
 
 const numberArea = document.querySelector("p")
 
-let clickedNumber = ""
+let clickedNumberLeft= ""
+let mathOperation = ""
+let clickedNumberRight = ""
 
-const checkValueOfButton = e => {
-    const buttonTarget = e.target
-    switch (buttonTarget.textContent) {
-        case "0":
-            if (numberArea.textContent !== "0") {
-                clickedNumber += "0"
+const rigthOrLeft = n => {
+    if (mathOperation === "") {
+        if (clickedNumberLeft === "") {
+            if (n !== "0") { 
+                clickedNumberLeft = n
             }
-            break
+        }
+        else {
+            clickedNumberLeft += n
+        }
+    } 
+    else {
+        if (clickedNumberRight === "") {
+            if (n !== "0") {
+                clickedNumberRight = n
+            }
+        }
+        else {
+            clickedNumberRight += n
+        }
+    }
+}
+
+const valueToMathOperation = m => {
+    if (mathOperation.length < 1 && clickedNumberLeft.length > 0) { // bug when i press +-/X itd when the numberArea is empty
+        mathOperation = m
+        console.log(mathOperation);
+    }
+}
+
+const reset = () => {
+    clickedNumberLeft = ""
+    mathOperation = ""
+    clickedNumberRight = ""
+}
+
+const equalTo = () => {
+    const left = parseFloat(clickedNumberLeft)
+    const right = parseFloat(clickedNumberRight)
+    let afterValue
+    switch (mathOperation) {
+        case "+":
+            afterValue = left + right
+        break
+        case "-":
+            afterValue = left - right
+        break
+        case "X":
+            afterValue = left * right
+        break
+        case "/":
+            if (right !== 0) {
+                afterValue = left / right
+            } else {
+                afterValue = "error"
+            }
+        break
+    }
+    reset()
+    clickedNumberLeft = afterValue.toString()
+    numberArea.textContent = afterValue.toString()
+}
+
+const btnFunctions = e => {
+    const btnText = e.target.textContent
+
+    switch (btnText) {
+        case "0":
+            rigthOrLeft("0")
+        break
         case "1":
-            clickedNumber += "1"
-            break
+            rigthOrLeft("1")
+        break
         case "2":
-            clickedNumber += "2"
-            break
+            rigthOrLeft("2")
+        break
         case "3":
-            clickedNumber += "3"
-            break
+            rigthOrLeft("3")
+        break
         case "4":
-            clickedNumber += "4"
-            break
+            rigthOrLeft("4")
+        break
         case "5":
-            clickedNumber += "5"
-            break
+            rigthOrLeft("5")
+        break
         case "6":
-            clickedNumber += "6"
-            break
+            rigthOrLeft("6")
+        break
         case "7":
-            clickedNumber += "7"
-            break
+            rigthOrLeft("7")
+        break
         case "8":
-            clickedNumber += "8"
-            break
+            rigthOrLeft("8")
+        break
         case "9":
-            clickedNumber += "9"
-            break
+            rigthOrLeft("9")
+        break
+        case "C": 
+            reset()
+        break
+        case "/":
+            valueToMathOperation("/")
+        break
+        case "X":
+            valueToMathOperation("X")
+        break
+        case "-":
+            valueToMathOperation("-")
+        break
+        case "+":
+            valueToMathOperation("+")
+        break
+        case "=":
+            equalTo()
+        break
     }
-
-    if (numberArea.textContent === "0") {
-        numberArea.textContent = buttonTarget.textContent
-    } else {
-        numberArea.textContent += buttonTarget.textContent
-    }
-    console.log(clickedNumber);
+    numberArea.textContent = `${clickedNumberLeft} ${mathOperation} ${clickedNumberRight}`
 }
 
-const resetCalculatorValues = () => {
-    numberArea.textContent = "0"
-    clickedNumber = ""
-}
 
-allNumbers.forEach(num => num.addEventListener("click", checkValueOfButton))
-btnReset.addEventListener("click", resetCalculatorValues)
+allButtons.forEach(num => num.addEventListener("click", btnFunctions))
